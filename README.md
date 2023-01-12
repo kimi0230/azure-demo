ee1071f44f35ff3ea98684ad8801b1cacbda618d
kimi <kimi0230@gmail.com>
2023-01-12 17:33:17 +0800

    feat: azure cli

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# azure-demo
+# azure - demo
+
+## MacOS 
+### install azure functions tool
+```sh
+brew tap azure/functions
+brew install azure-functions-core-tools@4
+brew link --overwrite azure-functions-core-tools@4
+```
+
+### Install az cli
+```sh
+brew update && brew install azure-cli
+```
+
+## Azure functions tool commands
+```sh
+func init
+func new
+func start &> ~/output.txt &
+
+# 測試 : 輸出是 6300
+curl "http://localhost:7071/api/simple-interest?principal=5000&rate=.035&term=36" -w "\n"
+```
+
+## Reference
+* [How to fix Azure functions: Incompatible Node.js version (v16.4.1)](https://stackoverflow.com/questions/70427342/how-to-fix-azure-functions-incompatible-node-js-version-v16-4-1)
+* [在 macOS 上安裝 Azure CLI](https://learn.microsoft.com/zh-tw/cli/azure/install-azure-cli-macos)
--- simple-interest/index.js
+++ simple-interest/index.js
@@ -1,13 +1,19 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  // Try to grab principal, rate, and term from the query string and
+  // parse them as numbers
+  const principal = parseFloat(req.query.principal);
+  const rate = parseFloat(req.query.rate);
+  const term = parseFloat(req.query.term);
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+  if ([principal, rate, term].some(isNaN)) {
+    // If any empty or non-numeric values, return a 400 response with an
+    // error message
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+      status: 400,
+      body: "Please supply principal, rate and term in the query string",
     };
-}
+  } else {
+    // Otherwise set the response body to the product of the three values
+    context.res = { body: principal * rate * term };
+  }
+};
